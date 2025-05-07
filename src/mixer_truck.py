b85a0f775fb323b1c53d3fec2005cdeadcfe169c
Alex <duch0l0@yandex.ru>
2025-05-07 18:00:44 +0300

    Миксера уходят с карты после отгрузки, проблема первого не решена

--- src/mixer_truck.py
+++ src/mixer_truck.py
@@ -6,11 +6,13 @@ class MixerTruck(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.Surface((50, 30))
-        self.image.fill((255, 165, 0))
-        self.speed = 2.5  # Оптимальная скорость
+        self.image.fill((255, 165, 0))  # Оранжевый цвет
+        self.speed = 1.5
         self.pouring = False
         self.pour_time = 0
         self.foundation = None
+        self.exiting = False  # Флаг для состояния ухода
+        self.exit_speed = 2.5  # Скорость ухода с площадки
         self._spawn_off_screen()
 
     def _spawn_off_screen(self):
@@ -26,13 +28,15 @@ class MixerTruck(pygame.sprite.Sprite):
             self.rect = self.image.get_rect(center=(-50, random.randint(0, SCREEN_HEIGHT)))
 
     def update(self):
-        if self.pouring:
-            self._handle_pouring()
+        if self.exiting:
+            self._handle_exiting()  # Обработка ухода с площадки
+        elif self.pouring:
+            self._handle_pouring()  # Обработка заливки
         else:
-            self._move_to_target()
+            self._move_to_target()  # Движение к цели
 
     def _move_to_target(self):
-        """Плавное движение с использованием get_distance()"""
+        """Движение к фундаменту с фиксированным радиусом остановки"""
         if not self.foundation:
             return
 
@@ -40,7 +44,7 @@ class MixerTruck(pygame.sprite.Sprite):
         current_pos = self.rect.center
         distance = get_distance(*current_pos, *target_pos)
         
-        if distance > 1:  # Остановка в 1px от цели
+        if distance > TARGET_OFFSET:
             dx = target_pos[0] - current_pos[0]
             dy = target_pos[1] - current_pos[1]
             self.rect.x += int(dx / distance * self.speed)
@@ -49,35 +53,50 @@ class MixerTruck(pygame.sprite.Sprite):
             self.start_pouring()
 
     def _handle_pouring(self):
-        """Обработка процесса заливки"""
+        """Процесс заливки бетона (3 секунды)"""
         self.pour_time += 1
-        if self.pour_time >= FPS * 3:  # Ровно 3 секунды
+        if self.pour_time >= FPS * 3:
             self._complete_pouring()
 
     def start_pouring(self):
+        """Начало заливки"""
         self.pouring = True
         self.pour_time = 0
 
     def _complete_pouring(self):
-        """Завершение заливки и уход с площадки"""
+        """Завершение заливки и начало ухода"""
         if self.foundation:
             self.foundation.concrete_amount = min(100, self.foundation.concrete_amount + 10)
-        self._exit_site()
+        self._start_exiting()
 
-    def _exit_site(self):
-        """Плавный уход за границы экрана"""
+    def _start_exiting(self):
+        """Подготовка к уходу с площадки"""
+        self.pouring = False
+        self.exiting = True
         # Выбираем случайную сторону для ухода
-        side = random.choice(['top', 'right', 'bottom', 'left'])
-        if side == 'top':
-            self.rect.y = -self.rect.height
-        elif side == 'right':
-            self.rect.x = SCREEN_WIDTH
-        elif side == 'bottom':
-            self.rect.y = SCREEN_HEIGHT
+        self.exit_side = random.choice(['top', 'right', 'bottom', 'left'])
+        # Устанавливаем высокую скорость для ухода
+        self.speed = self.exit_speed
+
+    def _handle_exiting(self):
+        """Процесс ухода с площадки"""
+        if self.exit_side == 'top':
+            self.rect.y -= self.exit_speed
+            if self.rect.bottom < 0:
+                self.kill()
+        elif self.exit_side == 'right':
+            self.rect.x += self.exit_speed
+            if self.rect.left > SCREEN_WIDTH:
+                self.kill()
+        elif self.exit_side == 'bottom':
+            self.rect.y += self.exit_speed
+            if self.rect.top > SCREEN_HEIGHT:
+                self.kill()
         else:  # left
-            self.rect.x = -self.rect.width
-        self.kill()
-    
+            self.rect.x -= self.exit_speed
+            if self.rect.right < 0:
+                self.kill()
+
     def set_target(self, foundation):
-        """Устанавливает цель (фундамент) для миксерного грузовика"""
+        """Установка цели (фундамента)"""
         self.foundation = foundation
